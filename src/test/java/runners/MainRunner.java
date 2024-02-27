94c6ead7c62613e2fbbee9bc9d3ba06441cbf0ee
Lungelo_Shabangu <lungelo.shabangu@ayo4u.com>
2024-02-27 15:52:39 +0200

    Update MainRunner.java

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -8,13 +8,6 @@ import org.testng.annotations.DataProvider;
         tags = "@contact-us", monochrome = true, dryRun = false,
         plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 public class MainRunner extends AbstractTestNGCucumberTests {
-    /*dry run doesnt run the selenium code in your steps, Runs the steps
-     * monochrome formats and makes code on the console readable
-     * plugin helps gives more info in the console
-     * webdriver
-     * webdriver123
-     * Validate successful and unsuccessful login
-     * */
     @Override
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
